f026e7c925c870263a44624deb368c71c191d687
zc <zachcaceres@gmail.com>
2019-04-25 03:06:23 +0000

    update install script. new sample audio. get closer to time warp

--- install.sh
+++ install.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-# ------------------------------------------------------------------
-# [FastAI SF Study Group] FastAI Audio Install Script
-# ------------------------------------------------------------------
 
-set -m
-# pip install pydub librosa fire --user
-# mkdir exp
-# sudo apt-get --assume-yes install ffmpeg sox libsox-dev libsox-fmt-all
+pip install fastai --user
 git clone https://github.com/pytorch/audio.git torchaudio
 cd torchaudio
 python setup.py install
